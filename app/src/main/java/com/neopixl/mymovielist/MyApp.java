6199b7fd0f8c3eb6e805e1b5b13a70c1bca1bcb2
Florian TIAR <contact@tiar-florian.fr>
2015-06-15 22:06:44 +0200

    Delete Image handler

--- app/src/main/java/com/neopixl/mymovielist/MyApp.java
+++ app/src/main/java/com/neopixl/mymovielist/MyApp.java
@@ -3,17 +3,13 @@ package com.neopixl.mymovielist;
 import android.app.Application;
 
 import com.android.volley.RequestQueue;
-import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.Volley;
-import com.neopixl.mymovielist.network.LruBitmapCache;
 
 public class MyApp extends Application {
 
     private static MyApp sharedInstance;
 
     private RequestQueue mVolleyRequestQueue;
-    private ImageLoader mVolleyImageLoader;
-
 
     @Override
     public void onCreate() {
@@ -23,7 +19,6 @@ public class MyApp extends Application {
 
         //Création de la queue
         mVolleyRequestQueue = Volley.newRequestQueue(getApplicationContext());
-        mVolleyImageLoader = new ImageLoader(mVolleyRequestQueue, new LruBitmapCache(1024 * 1024 * 1));
 
         //Démarrage de la queue
         mVolleyRequestQueue.start();
