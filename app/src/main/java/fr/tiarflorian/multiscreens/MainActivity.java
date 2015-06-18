0f874a7e5270f34334625fb98eeb57cf83b7243d
Florian TIAR <contact@tiar-florian.fr>
2015-06-18 12:10:58 +0200

    Prepare to final commit

--- app/src/main/java/fr/tiarflorian/multiscreens/MainActivity.java
+++ app/src/main/java/fr/tiarflorian/multiscreens/MainActivity.java
@@ -1,6 +1,7 @@
 package fr.tiarflorian.multiscreens;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
